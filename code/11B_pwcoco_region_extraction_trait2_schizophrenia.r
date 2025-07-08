13e6abed77f299dfa3f55a51ab550d1d185146b3
Christina <57257821+ChristinaDni@users.noreply.github.com>
2025-07-08 07:32:36 +0200

    Create 11B_pwcoco_region_extraction_trait2_schizophrenia.r

---
+++ code/11B_pwcoco_region_extraction_trait2_schizophrenia.r
@@ -0,0 +1,96 @@
+library(dplyr)
+library(readr)
+
+setwd("/dir/revision_03_07_25/pwcoco/trait/data")
+data<- read.table("topregions_asthma_schizophrenia.txt", sep="\t", header=T)
+
+ID<- data %>% split(x=data, f=data$SNP)
+
+#Read in the GWAS data to extract the regions
+gwas_data<- read_table("/dir/data_repo/clean/neuropsychiatric/B37:SCHIZOPHRENIA:2022:EUR.txt")
+
+gwas_data<- as.data.frame(gwas_data)
+
+#Here bipolar
+
+lapply(list(ID), function(x){
+  
+  for(x in ID) {
+
+#Define chromosome, position and window
+#These are necessary for extraction and file naming
+
+snp<- x$SNP
+  
+chr<- x$chr.exposure
+print(chr)
+
+pos<- x$pos.exposure
+print(pos)
+
+window<- 500000
+
+region_chr<-gwas_data[which(gwas_data$CHR==chr),] 
+region<- region_chr[which(region_chr$BP>=max(pos-window,0) & region_chr$BP<=pos+window),] 
+
+#Select the columns I need for pwcoco
+region<- region%>%dplyr::select("SNP","A1","A2", "FRQ", "logOR", "SE", "P", "NTOT", "NCAS")
+
+
+#Create a name for the file
+
+setwd("/dir/revision_03_07_25/pwcoco/trait/data/schizophrenia/trait2")
+
+file_out_snp<- paste0("scz","_", snp, ".txt")
+print(file_out_snp)
+
+write.table(region, file_out_snp, sep="\t", quote=F, row.names=F, col.names=T)
+
+}})
+
+setwd("/dir/revision_03_07_25/pwcoco/trait/data")
+data<- read.table("topregions_scz_asthma.txt", sep="\t", header=T)
+
+ID<- data %>% split(x=data, f=data$SNP)
+
+#Read in the GWAS data to extract the regions
+gwas_data<- read_table("/dir/data_repo/clean/respiratory/B37:ASTHMA:2022:EUR.txt")
+
+gwas_data<- as.data.frame(gwas_data)
+
+#Here bipolar
+
+lapply(list(ID), function(x){
+  
+  for(x in ID) {
+
+#Define chromosome, position and window
+#These are necessary for extraction and file naming
+
+snp<- x$SNP
+  
+chr<- x$chr.exposure
+print(chr)
+
+pos<- x$pos.exposure
+print(pos)
+
+window<- 500000
+
+region_chr<-gwas_data[which(gwas_data$CHR==chr),] 
+region<- region_chr[which(region_chr$BP>=max(pos-window,0) & region_chr$BP<=pos+window),] 
+
+#Select the columns I need for pwcoco
+region<- region%>%dplyr::select("SNP","A1","A2", "FRQ", "logOR", "SE", "P", "NTOT", "NCAS")
+
+
+#Create a name for the file
+
+setwd("/dir/revision_03_07_25/pwcoco/trait/data/schizophrenia/trait2")
+
+file_out_snp<- paste0("asthma","_", snp, ".txt")
+print(file_out_snp)
+
+write.table(region, file_out_snp, sep="\t", quote=F, row.names=F, col.names=T)
+
+}})
